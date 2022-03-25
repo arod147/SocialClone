f8553a67b0af141e81e93232803eff8c2841c8ef
arod147 <code.alex.rodriguez@outlook.com>
2022-03-25 14:56:27 -0400

    Added all folders/files for project

--- client/src/Components/HomePage/Homepage.jsx
+++ client/src/Components/HomePage/Homepage.jsx
@@ -1,5 +1,17 @@
+import Footer from "../Footer/Footer";
+import Form from "../Form/Form";
+import Header from "../Header/Header";
+import Posts from "../Posts/Posts";
+
 const Homepage = () => {
-  return <div>Homepage</div>;
+  return (
+    <>
+      <Header />
+      <Posts />
+      <Form />
+      <Footer />
+    </>
+  );
 };
 
 export default Homepage;
--- client/src/index.jsx
+++ client/src/index.jsx
@@ -3,8 +3,8 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import { store } from "./app/store";
 import { Provider } from "react-redux";
-import "bootstrap/dist/css/bootstrap.min.css";
 import { BrowserRouter } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 ReactDOM.render(
   <React.StrictMode>
